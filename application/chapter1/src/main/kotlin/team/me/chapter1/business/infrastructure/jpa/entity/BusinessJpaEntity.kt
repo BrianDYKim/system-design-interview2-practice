05747eefa8757a3408e9036a6d0b5dd5ec925101
BrianDYKim <18k7102dy@gmail.com>
2024-05-16 20:03:34 +0900

    jpa entity 정의 완료

--- application/chapter1/src/main/kotlin/team/me/chapter1/business/infrastructure/jpa/entity/BusinessJpaEntity.kt
+++ application/chapter1/src/main/kotlin/team/me/chapter1/business/infrastructure/jpa/entity/BusinessJpaEntity.kt
@@ -1,7 +1,49 @@
 package team.me.chapter1.business.infrastructure.jpa.entity
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+
 /**
  * @author Doyeop Kim
  * @since 2024. 5. 16.
  */
-class BusinessJpaEntity
+@Entity
+@Table(name = "businesses")
+open class BusinessJpaEntity protected constructor() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @get:Column(name = "id", nullable = false)
+    var id: Long = 0L
+
+    @get:Column(name = "address", nullable = false)
+    var address: String = ""
+
+    @get:Column(name = "city", nullable = false)
+    var city: String = ""
+
+    @get:Column(name = "state", nullable = false)
+    var state: String = ""
+
+    @get:Column(name = "country", nullable = false)
+    var country: String = ""
+
+    @get:Column(name = "latitude", nullable = false)
+    var latitude: Double = 0.0
+
+    @get:Column(name = "longitude", nullable = false)
+    var longitude: Double = 0.0
+
+    @get:Column(name = "created_at", nullable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now()
+
+    @get:Column(name = "updated_at", nullable = true)
+    var updatedAt: LocalDateTime? = null
+
+    @get:Column(name = "deleted_at", nullable = true)
+    var deletedAt: LocalDateTime? = null
+}
